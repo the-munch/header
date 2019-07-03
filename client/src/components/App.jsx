0a13788f5e3eeaf119fb23975a20eaf6ee6821d4
Kenny <kennyhojinlee@gmail.com>
2019-07-03 16:52:50 -0700

    set up webpack and server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default App;
